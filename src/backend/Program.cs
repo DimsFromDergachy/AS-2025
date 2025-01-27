3c8f0f6386b1e27f6cfff9626d8520ec769e8bf9
Одинцов Антон Александрович <an.odintsov@ase-ec.ru>
2025-01-28 02:00:45 +0300

    some workaround processes

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,6 +1,17 @@
+using AS_2025.Repository;
+using FastEndpoints;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddFastEndpoints();
+
+builder.Services.AddSingleton<ITraitRepository, TraitRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.UseFastEndpoints();
 
 app.Run();
