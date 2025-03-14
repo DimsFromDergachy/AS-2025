449d1e65b90727bd11e2b2270547bb8483a73aa9
Одинцов Антон Александрович <an.odintsov@ase-ec.ru>
2025-03-14 13:53:16 +0300

    team list item restore

--- src/backend/Api/Team/List/ListTeamsItem.cs
+++ src/backend/Api/Team/List/ListTeamsItem.cs
@@ -1,4 +1,5 @@
-﻿using AS_2025.Schema.List;
+﻿using AS_2025.Domain.Common;
+using AS_2025.Schema.List;
 
 namespace AS_2025.Api.Team.List;
 
@@ -9,4 +10,22 @@ public record ListTeamsItem
 
     [ListColumnSchema(VisibilityType = ListColumnVisibilityType.Hidden, DisplayType = ListColumnDisplayType.None)]
     public string Url { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.String, Order = 1)]
+    public string Name { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.Tag, TagReferenceEnum = nameof(TeamType), Order = 2)]
+    public string Type { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.String, Order = 3)]
+    public string DepartmentName { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.String, Order = 4)]
+    public string TeamLead { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.Integer, Order = 5)]
+    public int MembersCount { get; init; }
+
+    [ListColumnSchema(DisplayType = ListColumnDisplayType.Integer, Order = 6)]
+    public int AssignedProjectsCount { get; init; }
 }
--- src/backend/Api/Team/Mapper.cs
+++ src/backend/Api/Team/Mapper.cs
@@ -7,10 +7,24 @@ namespace AS_2025.Api.Team;
 public static partial class Mapper
 {
     [MapProperty(nameof(Domain.Entities.Team.Id), nameof(ListTeamsItem.Url), Use = nameof(GuidToUrl))]
+    [MapProperty("Department.Name", "DepartmentName")]
+    [MapProperty("TeamLead.FullName", "TeamLead")]
+    [MapProperty("Members", "MembersCount", Use = nameof(MembersToCount))]
+    [MapProperty("AssignedProjects", "AssignedProjectsCount", Use = nameof(AssignedProjectsToCount))]
     public static partial ListTeamsItem ToListItem(Domain.Entities.Team entity);
 
     private static string GuidToUrl(Guid id)
     {
         return $"/team/{id}";
+    }
+
+    private static int MembersToCount(List<Domain.Entities.Employee> members)
+    {
+        return members.Count;
+    }
+
+    private static int AssignedProjectsToCount(List<Domain.Entities.Project> projects)
+    {
+        return projects.Count;
     }
 }
