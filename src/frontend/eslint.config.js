871be1188b44af108e7bf0bf7f4601c02396c5d4
Якушенко Николай Юрьевич <N.Yakushenko@ase-ec.ru>
2025-03-15 10:39:36 +0300

    proxy test

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,9 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
+  {
+    env: {
+      node: true,
+    },
+  },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
@@ -30,4 +35,4 @@ export default [
       ],
     },
   },
-]
+];
