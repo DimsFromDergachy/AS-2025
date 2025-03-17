761065914e1b0b29cc324755c2eae8b8ad9072fd
Одинцов Антон Александрович <an.odintsov@ase-ec.ru>
2025-03-18 02:31:05 +0300

    restore mappers

--- src/backend/Api/Team/Mapper.cs
+++ src/backend/Api/Team/Mapper.cs
@@ -1,4 +1,6 @@
 ﻿using AS_2025.Api.Team.List;
+using AS_2025.Common;
+using AS_2025.Domain.Common;
 using Riok.Mapperly.Abstractions;
 
 namespace AS_2025.Api.Team;
@@ -7,10 +9,30 @@ namespace AS_2025.Api.Team;
 public static partial class Mapper
 {
     [MapProperty(nameof(Domain.Entities.Team.Id), nameof(ListTeamsItem.Url), Use = nameof(GuidToUrl))]
+    [MapProperty("Type", "Type", Use = nameof(TeamTypeToString))]
+    [MapProperty("Department.Name", "DepartmentName")]
+    [MapProperty("TeamLead.FullName", "TeamLead")]
+    [MapProperty("Members", "MembersCount", Use = nameof(MembersToCount))]
+    [MapProperty("AssignedProjects", "AssignedProjectsCount", Use = nameof(AssignedProjectsToCount))]
     public static partial ListTeamsItem ToListItem(Domain.Entities.Team entity);
 
     private static string GuidToUrl(Guid id)
     {
         return $"/team/{id}";
+    }
+
+    private static string TeamTypeToString(TeamType teamType)
+    {
+        return teamType.GetStringValue();
+    }
+
+    private static int MembersToCount(List<Domain.Entities.Employee> members)
+    {
+        return members.Count;
+    }
+
+    private static int AssignedProjectsToCount(List<Domain.Entities.Project> projects)
+    {
+        return projects.Count;
     }
 }
