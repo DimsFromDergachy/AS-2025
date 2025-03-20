317eec3f2daeccbc2f6edccf8ae9eacff1f24968
Якушенко Николай Юрьевич <N.Yakushenko@ase-ec.ru>
2025-03-20 17:37:31 +0300

    loader fix

--- src/frontend/src/api/client.js
+++ src/frontend/src/api/client.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { authService } from 'src/shared/Auth/authService';
 import { globalStore } from 'src/stores/globalStore';
 
+let activeRequests = 0;
+
 export const apiClient = axios.create({
   baseURL: '/api',
   timeout: 10000,
@@ -12,6 +14,7 @@ export const apiClient = axios.create({
 
 // Перехватчик для запросов
 apiClient.interceptors.request.use(config => {
+  activeRequests++;
   globalStore.loading.set(true);
   const token = authService.restore();
   if (token) {
@@ -20,17 +23,26 @@ apiClient.interceptors.request.use(config => {
   return config;
 });
 
-// Перехватчик для ошибок
+// Для уменьшения счетчика и скрытия лоадера
+const decrementRequests = () => {
+  activeRequests = Math.max(0, activeRequests - 1);
+  if (activeRequests === 0) {
+    globalStore.loading.set(false);
+  }
+};
+
+// Перехватчик для ответов
 apiClient.interceptors.response.use(
   response => {
-    globalStore.loading.set(false);
+    decrementRequests();
     return response.data;
   },
   error => {
+    decrementRequests();
     const [message = error.message] = Object.values(
       error.response?.data?.errors || {}
     );
-    globalStore.merge({ loading: false, serverError: { message } });
+    globalStore.merge({ serverError: { message } });
     return Promise.reject(new Error(message));
   }
 );
