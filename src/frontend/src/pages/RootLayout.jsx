b97af0fe9dc8f794a07c6af69c599cdce5a55dd3
Одинцов Антон Александрович <an.odintsov@ase-ec.ru>
2025-03-21 15:27:47 +0300

    ws workaround

--- src/frontend/src/pages/RootLayout.jsx
+++ src/frontend/src/pages/RootLayout.jsx
@@ -14,7 +14,7 @@ const { Header, Content, Sider } = Layout;
 
 const connectToHub = () => {
   const connection = new HubConnectionBuilder()
-    .withUrl('/api-events')
+    .withUrl('https://103.90.72.212:5004/api-events')
     .withAutomaticReconnect()
     .configureLogging(LogLevel.Information)
     .build();
